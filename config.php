02741aefceb0ebef328173331fbc83180ac02ad8
Venu Gopal <tvgece@gmail.com>
2012-05-10 19:10:44 +0530

    config file modified

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors',1);
 
-define('FB_APP_ID', 234006496703919);
-define('FB_APP_SECRET', '711992d9eb13f27fc4a23863162ee5ab');
+define('FB_APP_ID', 'XXXXXXXXXX');
+define('FB_APP_SECRET', 'XXXXXXXXXXXXXXXX');
 
 define('VERIFY_TOKEN', 'venu'); 
 
